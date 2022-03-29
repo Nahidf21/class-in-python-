dabe946f469b512ed94d912fd389b26307779236
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-28 21:40:10 -0500

    __init__()
    #creat a class named person, use the__init__() function to assign values for name and age:

--- 02.py
+++ 02.py
@@ -1,1 +1,8 @@
-#creat a class named person, use the__init__() function
+#creat a class named person, use the__init__() function to assign values for name and age:
+
+class Person:
+    def __init__(self, name , age):
+        self.name=name
+        self.age=age
+p1=Person("John",36)
+print(f"name is {p1.name} and age {p1.age}")
