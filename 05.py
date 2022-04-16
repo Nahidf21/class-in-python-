541e3b0649a57e263968251cf4d568a3096262fa
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-04-15 23:11:45 -0500

    add

--- 05.py
+++ 05.py
@@ -5,10 +5,9 @@ class Person:
 
   def myfunc(abc):
     print("Hello my name is " + abc.name)
-    print(f"Hello my age is {abc.age}" )
+    
     
 p1 = Person("John", 36)
-p1.age=40
-p2=Person("nahid",32)
-p1.myfunc()
-p2.myfunc()
+#modify Object Propertices
+del p1.age
+
